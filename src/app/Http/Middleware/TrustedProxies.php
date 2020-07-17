378424c8e54b1fe2ffe3aac81873221b89d9e824
chrisk <reclan@mail.ru>
2020-07-17 20:32:33 +0300

    AddTrustedProxies

---
+++ src/app/Http/Middleware/TrustedProxies.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
+}
